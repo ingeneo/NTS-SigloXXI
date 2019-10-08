6e844bf431eaf678562fd79e37d7dd1e81d31fd1
ingeneo <roger.raptor@gmail.como>
2019-10-07 20:32:59 -0500

    Lista estantes

--- lista_estantes.php
+++ lista_estantes.php
@@ -3,21 +3,21 @@
 $fecha = date('Y-m-d');
 $anio = date("Y", strtotime($fecha));
 
-function ListaCiudad() {
+function ListaEstantes() {
 	include_once 'conexion/conexion.php';
 	$objeto = new Conexion();
 	$conexion = $objeto->Conectar();
-	$depto = $_POST['depto'];
-	$consulta = "SELECT id_municipio AS ID, municipio AS CIUDAD 
-				FROM municipios WHERE Departamento_id_departamento = $depto AND estado = 1 
-				ORDER BY municipio ASC";
+	$bodega = $_POST['bodega'];
+	$consulta = "SELECT id_estante AS ID 
+				FROM estante WHERE Bodega_id_bodega = $bodega 
+				ORDER BY id_estante ASC";
 	$resultado = $conexion->prepare($consulta);
 	$resultado->execute();
-	$lista_ciudad = '<option value="" disabled selected>Seleccione la Ciudad...</option>';
+	$lista_estantes = '<option value="" disabled selected>Seleccione el estante...</option>';
 	while($row = $resultado->fetch(PDO::FETCH_ASSOC)) {
-		$lista_ciudad .= "<option value = '$row[ID]'>$row[CIUDAD]</option>";
+		$lista_estantes .= "<option value = '$row[ID]'>$row[ID]</option>";
 	}
-	return $lista_ciudad;
+	return $lista_estantes;
 }
-echo ListaCiudad();
+echo ListaEstantes();
 ?>
